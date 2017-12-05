fcea1bfc7deb326f3875cd61c80473af324d61c6
vally <valeria.leonardi@codiceplastico.com>
2017-12-05 10:08:52 +0100

    WIP aggiunta web server and get route

---
+++ server.js
@@ -0,0 +1,27 @@
+'use strict'
+
+var Fastify = require('fastify')
+var service = require('./service')
+
+var opts = {
+    port: 3000,
+    verbose: true,
+    logger: {level: 'info'}
+}
+
+
+    const app = Fastify(opts)
+    app.register(service, opts)
+    app.listen(opts.port, (err) => {
+      if (err) {
+        throw err
+      }
+    
+      app.log.info('server listening on port %d', app.server.address().port)
+    })
+
+
+
+
+
+
